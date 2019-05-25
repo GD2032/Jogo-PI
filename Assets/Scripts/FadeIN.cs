343e51f269c56099008f3d040d1e8b1658ccadda
Gabriel Aragao <gabrielaragaofds@gmail.com>
2019-05-25 11:40:47 -0300

    Versão FadeIN

---
+++ Assets/Scripts/FadeIN.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIN : MonoBehaviour
+{
+    [SerializeField]
+    GameObject fundo;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if ( fundo.tag =="Finish")
+        {
+            if (Time.time >= 3)
+            {
+                Destroy(fundo);
+            }
+        }
+        if (Time.time >= 10)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
